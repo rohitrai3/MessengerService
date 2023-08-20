ffa2417739d27d4b8225ca33c24898f5fafa48c3
Rohit Rai <raistudying@gmail.com>
2023-08-21 00:11:33 +0530

    Final code

--- src/main/java/com/messenger/MessengerService/MessengerServiceConfiguration.java
+++ src/main/java/com/messenger/MessengerService/MessengerServiceConfiguration.java
@@ -3,22 +3,30 @@ package com.messenger.MessengerService;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 
 @Configuration
+@Log4j2
 public class MessengerServiceConfiguration {
 
     @Bean
     public void initializeFirebase() throws IOException {
-        System.out.println("Initializing Firebase...");
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.getApplicationDefault())
-                .setDatabaseUrl("https://messenger-f035e-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        log.info("Initializing Firebase...");
+        Resource serviceAccountKey = new ClassPathResource("static/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccountKey.getInputStream()))
+                .setDatabaseUrl("https://messenger-f035e-default-rtdb.asia-southeast1.firebasedatabase.app")
                 .build();
+
         FirebaseApp.initializeApp(options);
-        System.out.println("...Firebase initialized.");
+
+        log.info("...Firebase initialized.");
     }
 }
--- src/main/java/com/messenger/MessengerService/controller/ChatController.java
+++ src/main/java/com/messenger/MessengerService/controller/ChatController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"https://messenger.rohitrai.dev", "https://beta.messenger.rohitrai.dev"})
 @RequestMapping("/chat")
 @RequiredArgsConstructor
 @RestController
