c348a4a4104bab3b70479c020710fa1bcabe5297
Rohit Rai <raistudying@gmail.com>
2023-08-28 16:02:32 +0530

    Add get-messages API

--- src/main/java/dev/rohitrai/messengerservice/service/ChatService.java
+++ src/main/java/dev/rohitrai/messengerservice/service/ChatService.java
@@ -2,15 +2,20 @@ package dev.rohitrai.messengerservice.service;
 
 import dev.rohitrai.messengerservice.dao.MessengerDao;
 import dev.rohitrai.messengerservice.model.AddMessageInput;
+import dev.rohitrai.messengerservice.model.GetMessagesOutput;
 import dev.rohitrai.messengerservice.model.MessageData;
 import dev.rohitrai.messengerservice.util.ChatNameFormatter;
+import dev.rohitrai.messengerservice.util.mapper.ObjectToMessageDataMapper;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -20,6 +25,8 @@ public class ChatService {
     private ChatNameFormatter chatNameFormatter;
     @NonNull
     private MessengerDao messengerDao;
+    @NonNull
+    private ObjectToMessageDataMapper objectToMessageDataMapper;
 
     public ResponseEntity<Void> addMessage(@NonNull AddMessageInput input) {
         MessageData messageData = input.getMessageData();
@@ -34,6 +41,20 @@ public class ChatService {
 
         return ResponseEntity.created(locationOfNewMessage)
                 .build();
+    }
+
+    public ResponseEntity<GetMessagesOutput> getMessages(@NonNull String sender, @RequestParam @NonNull String receiver) {
+        String chatName = chatNameFormatter.getChatName(sender, receiver);
+
+        List<Object> messageDataObjectList = messengerDao.readList("chats/" + chatName);
+
+        List<MessageData> messageDataList = messageDataObjectList.stream()
+                .map(objectToMessageDataMapper::map)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(GetMessagesOutput.builder()
+                .messageDataList(messageDataList)
+                .build());
     }
 
 }
