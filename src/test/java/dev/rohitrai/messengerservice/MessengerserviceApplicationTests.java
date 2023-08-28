8038f631cbd18a7d2c68f86cc6122f56b38eabcb
Rohit Rai <raistudying@gmail.com>
2023-08-28 09:18:44 +0530

    Initial project setup

--- src/test/java/dev/rohitrai/messengerservice/MessengerserviceApplicationTests.java
+++ src/test/java/dev/rohitrai/messengerservice/MessengerserviceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.messenger.MessengerService;
+package dev.rohitrai.messengerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MessengerServiceApplicationTests {
+class MessengerserviceApplicationTests {
 
 	@Test
 	void contextLoads() {
